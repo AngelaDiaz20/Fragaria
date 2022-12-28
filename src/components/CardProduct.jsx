0cf5fdc54e844c63272dde5028193dc58af1de22
Angela Diaz <angeldi846@gmail.com>
2022-12-27 21:28:27 -0500

    card animations and transitions

--- src/components/CardProduct.jsx
+++ src/components/CardProduct.jsx
@@ -1,9 +1,15 @@
 import Button from "./Button";
+import { RiSearchEyeLine } from 'react-icons/ri'
 
 function CardProduct({name, price, image}) {
     return (
         <div className="card_product">
-            <img src={image} alt={name}/>
+            <div>
+                <img src={image} alt={name}/>
+                <div className="quick_view">
+                    <RiSearchEyeLine className="icon"/> Ver más
+                </div>
+            </div>
             <h2>{name}</h2>
             <p>{price}</p>
             <Button text="Agregar" classe="button_product"/>
