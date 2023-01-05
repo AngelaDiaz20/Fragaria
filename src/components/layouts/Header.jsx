cd9f6bcfea711994afb4de3c2f68b769d202f492
Angela Diaz <angeldi846@gmail.com>
2023-01-05 01:51:02 -0500

    update responsive home page

--- src/components/layouts/Header.jsx
+++ src/components/layouts/Header.jsx
@@ -16,20 +16,22 @@ const Header = () => {
 
   return (
     <header className="header" >
-      <Link to='/home' className="container_logo"><img src={Logo} alt="Logo de Fragaria" /></Link>
-            <Link to='/cart' className='link_icon'>
-                    <FaShopify className='icon_shop'/>
-                    <span className='item_total'>{totalQuantity}</span>
-            </Link>
-            < MdMenuOpen className='icon_menu' />
-            <nav>
-                <NavLink to='/home' activeClassName='link_active'>Inicio</NavLink>
-                <NavLink to='/products' activeClassName='link_active'>Productos</NavLink>
-                <NavLink to='/cart' activeClassName='link_active' className='link_icon'>
-                    <FaShopify className='icon_shop'/>
-                    <span className='item_total'>{totalQuantity}</span>
-                </NavLink>
-            </nav> 
+        <Link to='/home' className="container_logo">
+            <img src={Logo} alt="Logo de Fragaria" />
+        </Link>
+        <Link to='/cart' className='link_icon cart_icon'>
+            <FaShopify className='icon_shop'/>
+            <span className='item_total'>{totalQuantity}</span>
+        </Link>
+        < MdMenuOpen className='icon_menu'/>
+        <nav>
+            <NavLink to='/home' activeClassName='link_active'>Inicio</NavLink>
+            <NavLink to='/products' activeClassName='link_active'>Productos</NavLink>
+            <NavLink to='/cart' activeClassName='link_active' className='link_icon'>
+                <FaShopify className='icon_shop'/>
+                <span className='item_total'>{totalQuantity}</span>
+            </NavLink>
+        </nav> 
     </header>
   );
 }; 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React, {useNavigate} from "react";
 import { Link } from "react-router-dom";
 
-import Store from '../assets/img/store.svg'
+import Store from '../assets/img/store.png'
 
 import Product1 from '../assets/img/products/product_1.jpg'
 import Product2 from '../assets/img/products/product_2.jpg'
@@ -30,10 +30,7 @@ const Home = () => {
               <h1>Detalles</h1>
             </div>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque dolores dolorum esse aliquam, laborum eos libero sed vel repellat eveniet nam officia fugiat minima. Laboriosam laborum dolor fugit velit beatae. <br /><br />
-              Assumenda quia eveniet facere corrupti alias, neque animi ut aliquid qui odit eaque non voluptates facilis illum nulla, accusamus harum, totam blanditiis quas dolore. Repellendus fugiat eligendi quis cupiditate numquam?</p></div>
-          {/* <svg viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
-        <path fill="#FF0066" d="M42.5,-64.5C48.9,-53.8,43.6,-33.1,49.4,-15.9C55.1,1.3,71.9,15,74.9,30.1C78,45.2,67.4,61.7,52.6,62.9C37.8,64.2,18.9,50.3,0.4,49.7C-18,49.1,-36.1,61.8,-48.5,59.8C-60.8,57.7,-67.5,40.9,-66.3,26C-65,11,-55.8,-2.1,-47.5,-11.6C-39.1,-21.2,-31.6,-27.2,-23.9,-37.4C-16.1,-47.7,-8,-62.2,5,-69.1C18,-76,36.1,-75.2,42.5,-64.5Z" transform="translate(100 100)" />
-      </svg> */}
+              Assumenda quia eveniet facere corrupti alias, neque animi ut aliquid qui odit eaque non voluptates facilis illum nulla, accusamus harum, totam blanditiis quas dolore. </p></div>
         </section>
 
         <section className='products_catalogue'>
